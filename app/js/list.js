93515f286193c1d57f71f6acd12ff5798783b3b7
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-12-02 18:29:57 -0200

    inserted function to work with chrome extension

--- app/js/list.js
+++ app/js/list.js
@@ -25,9 +25,23 @@ var List = {
   get: function() {
     var url = this.url;
 
+    var updateLinks = function() {
+      var elements = document.getElementsByTagName('a');
+      var lists = [].slice.call(elements);
+
+      lists.forEach(function(element, index) {
+        element.addEventListener('click', function(e) {
+          e.preventDefault();
+          chrome.tabs.create( { url: element.href } );
+        }, false);
+      });
+    };
+
     var onSuccess = function(datas) {
       var html = Template.compile(this.html, datas);
       this.element.innerHTML = html;
+
+      updateLinks();
     };
 
     Ajax.get({
