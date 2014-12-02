8ca22c3caf6ee1c7903884df7b075f6558c8a2f6
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-12-02 16:56:27 -0200

    added instance tests to humanize component

---
+++ test/spec.humanize-date.js
@@ -0,0 +1,18 @@
+var HumanizeDate = require('../app/js/humenize-date.js');
+
+describe('template', function() {
+  describe('instance', function() {
+    it('should to be a object', function() {
+      var isObject = typeof(HumanizeDate) === 'object';
+      expect(isObject).toBeTruthy();
+    });
+
+    it('should has timestamp method', function() {
+      expect(HumanizeDate.timestamp).toDefined();
+    });
+
+    it('should has update method', function() {
+      expect(HumanizeDate.update).toDefined();
+    });
+  });
+});
