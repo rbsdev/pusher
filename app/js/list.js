686d66cf18a7181db6f4f5c000653c7d3f6b94e5
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-12-02 11:28:28 -0200

    migrate ajax and template codes to news modules

--- app/js/list.js
+++ app/js/list.js
@@ -1,39 +1,6 @@
-var Ajax = {
-  get: function(params) {
-    var request = new XMLHttpRequest();
-    request('GET', params.url, false);
+var Ajax = require('./ajax.js');
+var Template = require('./template.js');
 
-    var onReady = function(e) {
-        var isReady = request.readyState == 4;
-
-        if (isReady) {
-            if (request.status == 200) {
-                params.success(request.response);
-                return false;
-            }
-
-            params.error();
-        }
-    };
-
-    request.onreadystatechange = onReady;
-  }
-};
-
-var Template = {
-  compile: function(html, datas) {
-    var output = '';
-
-    datas.forEach(function(element, index) {
-      output += html.replace('{{ SRC }}', datas.src);
-      output += html.replace('/{{ URL }}/gi', datas.url);
-      output += html.replace('{{ TITLE }}', datas.title);
-      output += html.replace('{{ DATE }}', datas.date);
-    });
-
-    return output;
-  }
-};
 
 var List = {
   url: 'http://172.20.92.64:8080/pusher-data-service/api/news/list/gremio?size=20&hl=1',
