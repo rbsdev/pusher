8ace252ef4d2b708a8def80bb8020e368f2644a2
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2014-12-02 18:32:04 -0200

    General adjustments

--- app/js/env.js
+++ app/js/env.js
@@ -3,7 +3,7 @@ var Env = {
   TEAM_SLUG: '{{ENVIRONMENT_TEAM_SLUG}}',
 
   service: {
-    NEWS: 'http://homologacaonovo.clicrbs.com.br/pusher-data-service/api/news/list/gremio?size=20&hl=1'
+    NEWS: 'http://zh.clicrbs.com.br/pusher-data-service/api/news/list/gremio?size=20&hl=1'
   }
 };
 
