b867c2222b1dddfdd6d26a8494ec26e1432bb1c9
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-12-02 11:20:10 -0200

    created compile to template

--- app/js/list.js
+++ app/js/list.js
@@ -23,6 +23,14 @@ var Ajax = {
 var Template = {
   compile: function(html, datas) {
     var output = '';
+
+    datas.forEach(function(element, index) {
+      output += html.replace('{{ SRC }}', datas.src);
+      output += html.replace('/{{ URL }}/gi', datas.url);
+      output += html.replace('{{ TITLE }}', datas.title);
+      output += html.replace('{{ DATE }}', datas.date);
+    });
+
     return output;
   }
 };
