5fa15a7e4be1bfdaf437bc9cd6952b829a922f55
isaiasa <isaiasa@gmail.com>
2014-12-03 16:40:55 -0200

    Peforming humanize messages

--- test/spec.humanize-date.js
+++ test/spec.humanize-date.js
@@ -19,7 +19,7 @@ describe('template', function() {
     
     it('should return exactly now timestamp value', function() {
         expect(HumanizeDate.update(Date.now())).toEqual( "Agora"   ); 
-     });
+    });
     
     it('should return exactly x minute ago value', function() {
        var scale = 24*60*60;
@@ -29,15 +29,17 @@ describe('template', function() {
     });
     
     it('should return exactly x hour ago value', function() {
-        expect(HumanizeDate.update(Date.now()-(24*60*60*60))).toEqual( "Há 1 hora"   ); 
+    	var scale = 24*60*60*60;
+        for (i=1; i < 3 ; i++) {
+        	expect(HumanizeDate.update(Date.now()-(scale)*i)).toEqual( "Há "+(i)+" hora"+(i==1?"":"s")); 
+        }
     });
     
     it('should return exactly x day ago value', function() {
-        expect(HumanizeDate.update(Date.now()-(24*60*60*60*24))).toEqual( "Há 1 dia"   ); 
-    });
-    
-    it('should return exactly x month ago value', function() {
-        expect(HumanizeDate.update(Date.now()-(24*60*60*60*24))).toEqual( "Há 1 dia"   ); 
+    	var scale = (24*60*60*60)*24;
+        for (i=1; i < 3 ; i++) {
+        	expect(HumanizeDate.update(Date.now()-(scale)*i)).toEqual( "Há "+(i)+" dia"+(i==1?"":"s")); 
+        }
     });
     
   });
