a91f5f50ab3fa4ca4d22f3ae9cc38127181f45a9
Gibran Malheiros <gibatronic@gmail.com>
2014-12-03 11:20:04 -0200

    fixed pack task to create images folder

--- gruntfile.js
+++ gruntfile.js
@@ -106,8 +106,10 @@ module.exports = function(grunt) {
             'mkdir -p build/chrome/{{BUILD_TEAM_SLUG}}/styles',
             'cp app/build/styles/{{BUILD_TEAM_SLUG}}.css build/chrome/{{BUILD_TEAM_SLUG}}/styles/main.css',
 
+            'mkdir -p build/chrome/{{BUILD_TEAM_SLUG}}/images',
             'cp -R app/images/zh.svg build/chrome/{{BUILD_TEAM_SLUG}}/images/',
             'cp -R app/images/{{BUILD_TEAM_SLUG}} build/chrome/{{BUILD_TEAM_SLUG}}/images/',
+
             'cp app/index.html build/chrome/{{BUILD_TEAM_SLUG}}/index.html',
 
             'mkdir -p build/chrome/{{BUILD_TEAM_SLUG}}/scripts',
@@ -129,8 +131,10 @@ module.exports = function(grunt) {
             'mkdir -p build/sandbox/{{BUILD_TEAM_SLUG}}/styles',
             'cp app/build/styles/{{BUILD_TEAM_SLUG}}.css build/sandbox/{{BUILD_TEAM_SLUG}}/styles/main.css',
 
+            'mkdir -p build/sandbox/{{BUILD_TEAM_SLUG}}/images',
             'cp -R app/images/zh.svg build/chrome/{{BUILD_TEAM_SLUG}}/images/',
             'cp -R app/images/{{BUILD_TEAM_SLUG}} build/sandbox/{{BUILD_TEAM_SLUG}}/images/',
+
             'cp app/index.html build/sandbox/{{BUILD_TEAM_SLUG}}/index.html',
 
             'mkdir -p build/sandbox/{{BUILD_TEAM_SLUG}}/scripts',
