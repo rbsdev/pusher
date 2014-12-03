38dca51af164b23162af2fad23cf607c9d806793
isaiasa <isaiasa@gmail.com>
2014-12-03 17:42:25 -0200

    New template test cases

--- test/spec.humanize-date.js
+++ test/spec.humanize-date.js
@@ -16,31 +16,34 @@ describe('template', function() {
       var isFunction = typeof(HumanizeDate.update) === 'function';
       expect(isFunction).toBeTruthy();
     });
-    
-    it('should return exactly now timestamp value', function() {
-        expect(HumanizeDate.update(Date.now())).toEqual( "Agora"   ); 
-    });
-    
-    it('should return exactly x minute ago value', function() {
-       var scale = 24*60*60;
-       for (i=1; i < 3 ; i++) {
-    	   expect(HumanizeDate.update(Date.now()-(scale)*i)).toEqual( "Há "+(i)+" minuto"+(i==1?"":"s")); 
-       }
-    });
-    
-    it('should return exactly x hour ago value', function() {
-    	var scale = 24*60*60*60;
-        for (i=1; i < 3 ; i++) {
-        	expect(HumanizeDate.update(Date.now()-(scale)*i)).toEqual( "Há "+(i)+" hora"+(i==1?"":"s")); 
-        }
-    });
-    
-    it('should return exactly x day ago value', function() {
-    	var scale = (24*60*60*60)*24;
-        for (i=1; i < 3 ; i++) {
-        	expect(HumanizeDate.update(Date.now()-(scale)*i)).toEqual( "Há "+(i)+" dia"+(i==1?"":"s")); 
-        }
-    });
-    
   });
+  
+  describe('update method', function() {
+	  
+	  it('should return exactly now timestamp value', function() {
+	        expect(HumanizeDate.update(Date.now())).toEqual( "Agora"   ); 
+	    });
+	    
+	    it('should return exactly x minute ago value', function() {
+	       var scale = 24*60*60;
+	       for (i=1; i < 3 ; i++) {
+	    	   expect(HumanizeDate.update(Date.now()-(scale)*i)).toEqual( "Há "+(i)+" minuto"+(i==1?"":"s")); 
+	       }
+	    });
+	    
+	    it('should return exactly x hour ago value', function() {
+	    	var scale = 24*60*60*60;
+	        for (i=1; i < 3 ; i++) {
+	        	expect(HumanizeDate.update(Date.now()-(scale)*i)).toEqual( "Há "+(i)+" hora"+(i==1?"":"s")); 
+	        }
+	    });
+	    
+	    it('should return exactly x day ago value', function() {
+	    	var scale = (24*60*60*60)*24;
+	        for (i=1; i < 3 ; i++) {
+	        	expect(HumanizeDate.update(Date.now()-(scale)*i)).toEqual( "Há "+(i)+" dia"+(i==1?"":"s")); 
+	        }
+	    });
+  });
+  
 });
