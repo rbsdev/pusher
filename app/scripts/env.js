147712b06f625477e039001733cda5f3b6c384ec
Gibran Malheiros <gibatronic@gmail.com>
2014-12-04 14:41:14 -0200

    added news from opponent service

--- app/scripts/env.js
+++ app/scripts/env.js
@@ -3,7 +3,8 @@ var Env = {
   TEAM_SLUG: '{{ENVIRONMENT_TEAM_SLUG}}',
 
   service: {
-    NEWS: 'http://zh.clicrbs.com.br/pusher-data-service/api/news/list/{{ENVIRONMENT_TEAM_SLUG}}?size=20&hl=1'
+    NEWS: 'http://zh.clicrbs.com.br/pusher-data-service/api/news/list/{{ENVIRONMENT_TEAM_SLUG}}?size=20&hl=1',
+    NEWS_FROM_OPPONENT: 'http://zh.clicrbs.com.br/pusher-data-service/api/news/list/{{ENVIRONMENT_OPPONENT_SLUG}}?size=20&hl=1'
   }
 };
 
@@ -18,20 +19,16 @@ Env.isSandboxKind = Env.KIND_SLUG == 'sandbox';
 Env.isWindowsKind = Env.KIND_SLUG == 'windows';
 
 if (Env.isGremioTeam) {
-  Env.TEAM_COLOR = {
-    array: [35, 145, 230, 255],
-    rgba: 'rgba(35, 145, 230, 1)'
-  };
-
+  Env.service.NEWS_FROM_OPPONENT = Env.service.NEWS_FROM_OPPONENT.replace('{{ENVIRONMENT_OPPONENT_SLUG}}', 'inter');
+  Env.TEAM_COLOR_ARRAY = [35, 145, 230, 255];
+  Env.TEAM_COLOR_RGBA = 'rgba(35, 145, 230, 1)';
   Env.TEAM_NAME = 'Grêmio';
   Env.TEAM_NICK = 'Gremista';
   Env.TEAM_LINK = 'http://zh.clicrbs.com.br/rs/esportes/gremio';
 } else if (Env.isInterTeam) {
-  Env.TEAM_COLOR = {
-    array: [221, 26, 49, 255],
-    rgba: 'rgba(221, 26, 49, 1)'
-  };
-
+  Env.service.NEWS_FROM_OPPONENT = Env.service.NEWS_FROM_OPPONENT.replace('{{ENVIRONMENT_OPPONENT_SLUG}}', 'gremio');
+  Env.TEAM_COLOR_ARRAY = [35, 145, 230, 255];
+  Env.TEAM_COLOR_RGBA = 'rgba(35, 145, 230, 1)';
   Env.TEAM_NAME = 'Inter';
   Env.TEAM_NICK = 'Colorado';
   Env.TEAM_LINK = 'http://zh.clicrbs.com.br/rs/esportes/inter';
