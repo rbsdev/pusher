817f5050b4cd56903f634e669269e9cc278d13fb
Gibran Malheiros <gibatronic@gmail.com>
2014-12-05 12:16:24 -0200

    improved tracker to handle Google Analytics SDK seteup

--- app/scripts/tracker.js
+++ app/scripts/tracker.js
@@ -1,6 +1,22 @@
 var Tracker = {
+  init: function() {
+    var sdk = document.createElement('script');
+
+    window._gaq = window._gaq || [];
+    window._gaq.push(['_setAccount', 'UA-3469445-1']);
+    window._gaq.push(['_setAllowHash', false]);
+    window._gaq.push(['_trackPageview']);
+    window._gaq.push(['_setAllowLinker', true]);
+
+    sdk.type = 'text/javascript';
+    sdk.async = true;
+    sdk.src = 'https://ssl.google-analytics.com/ga.js';
+
+    document.body.appendChild(sdk);
+  },
+
   trigger: function() {
-    _gaq.push(['_trackEvent', 'Cliques Extensoes - ZH'].concat([ ].slice.call(arguments)));
+    window._gaq.push(['_trackEvent', 'Cliques Extensoes - ZH'].concat([ ].slice.call(arguments)));
   }
 };
 
