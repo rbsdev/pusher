339fb014f4d1c1ea53dc7bcee874efd6fc8fa65f
Gibran Malheiros <gibatronic@gmail.com>
2014-12-05 16:42:05 -0200

    disabled share for now

--- app/scripts/share.js
+++ app/scripts/share.js
@@ -1,32 +1,32 @@
-var Share = function(url) {
-  return {
-    url: url,
+// var Share = function(url) {
+//   return {
+//     url: url,
 
-    events: function() {
-      var elements = document.querySelectorAll('.share a');
-      var lists = [].slice.call(elements);
+//     events: function() {
+//       var elements = document.querySelectorAll('.share a');
+//       var lists = [].slice.call(elements);
 
-      var onClick = function(e) {
-        e.preventDefault();
+//       var onClick = function(e) {
+//         e.preventDefault();
 
         
-      };
+//       };
 
-      list.forEach(function(element, index) {
-        element.addEventListener('click', onClick);
-      });
-    },
+//       list.forEach(function(element, index) {
+//         element.addEventListener('click', onClick);
+//       });
+//     },
 
-    twitter: function() {
-      var urlTwitter = 'https://twitter.com/share?url={{ URL }}'.replace('{{ URL }}', url);
-      window.open(urlTwitter);
-    },
+//     twitter: function() {
+//       var urlTwitter = 'https://twitter.com/share?url={{ URL }}'.replace('{{ URL }}', url);
+//       window.open(urlTwitter);
+//     },
 
-    facebook: function() {
-      var urlFacebook = 'https://www.facebook.com/sharer/sharer.php?u={{ URL }}'.replace('{{ URL }}', url);
-      window.open(urlFacebook);
-    }
-  }
-};
+//     facebook: function() {
+//       var urlFacebook = 'https://www.facebook.com/sharer/sharer.php?u={{ URL }}'.replace('{{ URL }}', url);
+//       window.open(urlFacebook);
+//     }
+//   }
+// };
 
-module.exports = Share;
+// module.exports = Share;
